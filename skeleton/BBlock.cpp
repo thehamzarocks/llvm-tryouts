dd403631daf128dc39985637f4c8f4abc58f74ff
thehamzarocks <thehamzarocks@gmail.com>
2017-10-31 16:45:31 +0530

    mapping instruction addresses to availability bits

--- skeleton/BBlock.cpp
+++ skeleton/BBlock.cpp
@@ -3,7 +3,11 @@
 #include "llvm/Support/raw_ostream.h"
 #include "llvm/IR/LegacyPassManager.h"
 #include "llvm/Transforms/IPO/PassManagerBuilder.h"
+#include "llvm/IR/BasicBlock.h"
+#include <map>
+#include <iterator>
 using namespace llvm;
+using namespace std;
 
 namespace {
   struct SkeletonPass : public FunctionPass {
@@ -11,17 +15,40 @@ namespace {
     SkeletonPass() : FunctionPass(ID) {}
 
 
+    struct node {
+	    Instruction *I;
+	    int available;
+    };
+
+    node *graph;
+
+    typedef std::map <Instruction*, int> avail;
+    avail avail_in;
+    avail avail_out;
+    avail::iterator it;
+
 
     virtual bool runOnFunction(Function &F) {
       errs() << "I saw a function called " << F.getName() << "!\n";
       F.dump();
+      graph = NULL;
+      node *ptr = graph;
       //return false;
       for(auto& B : F) {
 	      errs()<< "I saw a basic block\n";
 	      Instruction  *TInst = (Instruction*) B.getTerminator();
-	      errs()<< (*TInst) << "is the terminator\n";
+	      //errs()<< (*TInst) << "is the terminator\n";
+	      //Instruction *I = (Instruction*) B.getFirstNonPHI();
+	      //errs()<< (*I) << "is the first instruction\n";
+	      for(auto& I : B) {
+		      avail_in.insert(make_pair(&I,0));
+	      }
+	      for (it = avail_in.begin(); it != avail_in.end(); ++it) {
+		      errs()<< it->first << " " << it->second << "\n";
+	      }
 
       }
+
       
     }
   };
